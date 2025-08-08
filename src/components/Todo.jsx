e8ab215022b6fd65197aceff9d622c2258ee3e44
saritadipa34 <saritadipa34@gmail.com>
2025-08-08 08:05:49 +0530

    localStorage added & delete logic modified

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin6Fill } from "react-icons/ri";
 
 const Todo=()=>{
-    const [tasks,setTasks]=useState([]);
+    const [tasks,setTasks]=useState(()=>{
+        const storedTasks=localStorage.getItem("localTodo");
+        return storedTasks ? JSON.parse(storedTasks) : [];
+    });
     const [inputValue,setInputValue]=useState("");
 
+useEffect(()=>{
+    console.log("before")
+localStorage.setItem("localTodo",JSON.stringify(tasks));
+console.log("setitem")
+},[tasks])
+
 const handleDelete=(i)=>{
-    const deleteTask=tasks[i];
-    const filtered=tasks.filter((task)=>{
-        return task[i] !== deleteTask[i];
-    });
+    const filtered=tasks.filter((_,index)=>{
+        return i !== index;
+    })
     setTasks(filtered);
+    console.log("delete");
 }
+
 
 const handleEdit=(i)=>{
     const oldTask=tasks[i];
@@ -45,10 +55,10 @@ const handleAdd=()=>{
              <input value={inputValue} onChange={handleInput} type="text" className=" px-2 outline-0 border-1 w-3/4 bg-white text-black h-10 rounded-l-lg" />
              <p onClick={handleAdd} className="h-10 flex justify-center items-center cursor-pointer bg-sky-400 w-1/4 text-white rounded-r-lg">Add Task</p>
              </div>
-        <div className="flex flex-col gap-4 overflow-y-hidden"> 
+        <div className="flex flex-col gap-4 overflow-y-auto "> 
             { 
-tasks.map((task,i)=>(
-    <div key={i} className="w-full flex" task={task}>
+tasks && tasks.map((task,i)=>(
+    <div key={i} className="w-full flex">
     <div className="px-2 w-3/4 min-h-10 bg-white rounded-l-lg text-black text-ellipsis overflow-hidden">
     {task}
     </div>
